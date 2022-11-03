2c0bd66a6053b875af9ff4d5a15068459486c730
Raul Sanchez <raul@um.es>
2022-11-03 12:56:09 +0000

    working. Remove punctuation of token =! PUNCT

--- daedalus_pipeline.py
+++ daedalus_pipeline.py
@@ -22,7 +22,7 @@ def align_audio_text(audio_file, transcription):
 
   with gentle.resampled(audio_file) as wavfile:
       print("starting alignment")
-      aligner = gentle.ForcedAligner(resources, transcription, nthreads=4, disfluency=False, conservative=False, disfluencies=False)
+      aligner = gentle.ForcedAligner(resources, transcription, nthreads=4, disfluency=False, conservative=True, disfluencies=False)
       result = aligner.transcribe(wavfile)
 
   return result
@@ -30,19 +30,19 @@ def align_audio_text(audio_file, transcription):
 
 # Get gentle's result and return a list of words with their start and end timestamps
 def get_word_timestamps(result):
-    word_timestamps = []
-    prev_start = 0 
-    prev_end = 0
-    for word in result.words:
-        if word.start is None:
-            word.start = prev_start        
-        if word.end is None:
-            word.end = prev_end
-        word_timestamps.append({'word': word.word, 'start': "{:.2f}".format(word.start), 'end': "{:.2f}".format(word.end)})
-        prev_start = word.start
-        prev_end = word.end
+  word_timestamps = []
+  prev_start = 0 
+  prev_end = 0
+  for word in result.words:
+      if word.start is None:
+          word.start = prev_start        
+      if word.end is None:
+          word.end = prev_end
+      word_timestamps.append({'word': word.word, 'start': "{:.2f}".format(word.start), 'end': "{:.2f}".format(word.end)})
+      prev_start = word.start
+      prev_end = word.end
 
-    return word_timestamps
+  return word_timestamps
 
 
 def get_secs(time):
@@ -64,39 +64,48 @@ def get_msecs(time):
 # Process transcription with spacy to get NLP features
 # and finally generate a vrt file with the name of the audio/video file + .vrt
 def generate_vrt(file, whisper_model):
-
   print('Loading whisper model...')
   model = whisper.load_model(whisper_model)
 
   print('Transcribing audio...')
   results = model.transcribe(file, max_initial_timestamp=None)
 
-  
 
-  aligned_results = align_audio_text(file,results['text'])
-  word_timestamps = get_word_timestamps(aligned_results) 
 
+  print('Loading spacy model...')
+  nlp = spacy.load('en_core_web_lg')
+  sentencizer = nlp.add_pipe("sentencizer")
+  doc = nlp(results['text'])
+
+  spacy_tokens = ""
+
+  for token in doc:
+    # if token is a number with , or . delete , or . 
+    if token.pos_ == 'NUM':
+      # delete puntuation from token
+      tmp = token.text.replace(',', '')
+      tmp = tmp.replace('.', '')
+      spacy_tokens += tmp + " "
+    else:
+      spacy_tokens += token.text + " "
+
+  aligned_results = align_audio_text(file,spacy_tokens) 
+  words_timestamps = get_word_timestamps(aligned_results)
 
   # write transcription to a file
   with open(file + '.txt', 'w') as f:
-    f.write(results['text'])
+    f.write(spacy_tokens)
 
   # close file
   f.close()
 
   # write word timestamps to a file
   with open(file + '.timestamps', 'w') as f:
-    for word in word_timestamps:
+    for word in words_timestamps:
       f.write(word['word'] + ' ' + word['start'] + ' ' + word['end'] + '\n')
   
   f.close()
 
-
-
-  print('Loading spacy model...')
-  nlp = spacy.load('en_core_web_lg')
-  doc = nlp(results['text'])
-  sentencizer = nlp.add_pipe("sentencizer")
 
 
 
@@ -114,35 +123,40 @@ def generate_vrt(file, whisper_model):
       sentence_id += 1
       vrt_file.write('<s id="' + str(sentence_id) + '" ' + 'file="' + filename + '" ' + ">\n")
       for token in sent:
+        if token.pos_ != 'PUNCT':
+          # delete puntuation from token
+          tmp = token.text.replace(',', '')
+          tmp = tmp.replace('.', '')
+          token_text = tmp
+        else:
+          token_text = token.text          
+
         # find the word timestamp for the token
         found = False
-        for word in word_timestamps:
-          # if word starts with token print token and timestamp or token start with word
-          #if token.text.startswith(word['word']) or word['word'].startswith(token.text):
-          # if word contains token or token contains word
-          if token.text in word['word'] or word['word'] in token.text:
-          #if word['word'].startswith(token.text):
+        for word in words_timestamps:
+          # if word starts with token print token and timestamp
+          if word['word'].startswith(token_text):
             found = True
             last_start = word['start']
             last_end = word['end']
             # vrt_file.write(token.text + "\t " + get_secs(word['start']) + " \t " + get_msecs(word['start']) + " \t " + get_secs(word['end']) + " \t " + get_msecs(word['end']) + "\n")
-            vrt_file.write(token.text + " \t " + token.lower_  +  " \t " + token.prefix_  +  " \t " + token.suffix_ + " \t " + 
+            vrt_file.write(token_text + " \t " + token.lower_  +  " \t " + token.prefix_  +  " \t " + token.suffix_ + " \t " + 
                           str(token.is_digit) + " \t " + str(token.like_num) + " \t " + token.dep_ + " \t " + token.shape_ + " \t " + 
                           token.lemma_ + " \t " +  token.pos_ + " \t " +  token.tag_ + " \t "  +  str(token.sentiment) + " \t " +
                           str(token.is_alpha) + " \t " +  str(token.is_stop) + " \t " +  token.head.text + " \t " +  
                           token.head.pos_ + " \t " +  str([child for child in token.children]) + " \t " + 
                           get_secs(word['start']) + " \t " + get_msecs(word['start']) + " \t " + get_secs(word['end']) + " \t " + get_msecs(word['end']) + "\n")
-            # delete all words until the used one
-            word_timestamps = word_timestamps[word_timestamps.index(word)+1:]
+              # delete all words until the used one
+            words_timestamps = words_timestamps[words_timestamps.index(word)+1:]
             break
           if not found:
             # vrt_file.write(token.text + " \t " + get_secs(last_start) + " \t " + get_msecs(last_start) + " \t " + get_secs(last_end) + " \t " + get_msecs(last_end) + "\n")
-            vrt_file.write(token.text + " \t " + token.lower_  +  " \t " + token.prefix_  +  " \t " + token.suffix_ + " \t " + 
-                          str(token.is_digit) + " \t " + str(token.like_num) + " \t " + token.dep_ + " \t " + token.shape_ + " \t " + 
-                          token.lemma_ + " \t " +  token.pos_ + " \t " +  token.tag_ + " \t "  +  str(token.sentiment) + " \t " +
-                          str(token.is_alpha) + " \t " +  str(token.is_stop) + " \t " +  token.head.text + " \t " +  
-                          token.head.pos_ + " \t " +  str([child for child in token.children]) + " \t " + 
-                          get_secs(last_start) + " \t " + get_msecs(last_start) + " \t " + get_secs(last_end) + " \t " + get_msecs(last_end) + "\n")
+            vrt_file.write(token_text + " \t " + token.lower_  +  " \t " + token.prefix_  +  " \t " + token.suffix_ + " \t " + 
+                            str(token.is_digit) + " \t " + str(token.like_num) + " \t " + token.dep_ + " \t " + token.shape_ + " \t " + 
+                            token.lemma_ + " \t " +  token.pos_ + " \t " +  token.tag_ + " \t "  +  str(token.sentiment) + " \t " +
+                            str(token.is_alpha) + " \t " +  str(token.is_stop) + " \t " +  token.head.text + " \t " +  
+                            token.head.pos_ + " \t " +  str([child for child in token.children]) + " \t " + 
+                            get_secs(last_start) + " \t " + get_msecs(last_start) + " \t " + get_secs(last_end) + " \t " + get_msecs(last_end) + "\n")
             break
       vrt_file.write("</s>\n")
   vrt_file.write("</text>\n")
